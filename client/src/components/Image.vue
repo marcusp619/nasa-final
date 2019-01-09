4554f8676490622020bf487582849685776f51b9
marcusp619 <marcusp619@gmail.com>
2019-01-09 10:00:58 -0700

    Add method to fetch photos

--- client/src/components/Image.vue
+++ client/src/components/Image.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="hello">
-    
+  <div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Image',
-}
+    name: 'Image',
+    data () {
+      return {
+        query: '',
+        images: [...images],
+        POTD: '',
+        displayedPic: ''
+      }
+    },
+    methods: {
+      async fetchPhotos() {
+        const response = await axios.get('/api/v1/images');
+        this.images = response.data.images
+      }
+    },
+
+    mounted() {
+      this.fetchPhotos()
+    }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
