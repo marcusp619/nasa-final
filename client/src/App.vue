5901a05df567f195f52c18476d9dce85dc18e610
marcusp619 <marcusp619@gmail.com>
2019-01-09 09:57:36 -0700

    Removed all scaffold code

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Image />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Image from './components/Image.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Image
   }
 }
 </script>
